5ea5ce9787a42a01243ce3c1c9b9c3897f58e425
Sérgio Fagundes <contato.sergiofagundes@gmail.com>
2024-07-16 18:39:44 -0300

    API node with postgres

---
+++ database-postgres.js
@@ -0,0 +1,33 @@
+import { sql } from './db.js'
+import { randomUUID } from "node:crypto"
+
+export class DatabasePostgres {
+    async list(search) {
+        let videos
+
+        if (search) {
+            videos = await sql`SELECT * FROM videos WHERE title ilike ${'%' + search + '%'}`
+        } else {
+            videos = await sql`SELECT * FROM videos`
+        }
+
+        return videos
+    }
+
+    async create(video) {
+        const videoId = randomUUID()
+        const { title, description, duration } = video
+
+        await sql`INSERT INTO videos (id, title, description, duration) VALUES (${videoId}, ${title}, ${description}, ${duration})`
+    }
+
+    async update(id, video) {
+        const { title, description, duration } = video
+
+        await sql`UPDATE videos SET tiles = ${title}, description = ${description}, duration = ${duration} WHERE id = ${id}`
+    }
+
+    async delete(id) {
+        await sql`DELETE FROM videos WHERE id = ${id}`
+    }
+}
